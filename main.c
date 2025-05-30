7e8037351798e3863461647dfa6cff32f1970bc4
Yassine El-Alja <yel-alja@c2r3p2.1337.ma>
2025-05-30 10:28:15 +0100

    separate tokenizer into small functions

--- main.c
+++ main.c
@@ -6,17 +6,27 @@
 /*   By: yel-alja <yel-alja@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:27:14 by yel-alja          #+#    #+#             */
-/*   Updated: 2025/05/29 22:04:25 by yel-alja         ###   ########.fr       */
+/*   Updated: 2025/05/30 09:52:09 by yel-alja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minishell.h"
 
+char *type_to_str(t_type type)
+{
+    if (type == PIPE) return "PIPE";
+    if (type == APPEND) return "APPEND";
+    if (type == OUTPUT) return "REDIR_OUT";
+    if (type == INPUT) return "REDIR_IN";
+    if (type == HEREDOC) return "HEREDOC";
+    return "WORD";
+}
+
 void print_tokens(t_token *token)
 {
     while(token)
     {
-        printf("{%s, %d}\n" , token->value, token->type);
+        printf("\033[36m[value : \033[0m\033[33m%s\033[0m\033[36m]    [type : \033[0m\033[32m%s\033[0m\033[36m]\033[0m\n",token->value, type_to_str(token->type));
         token = token->next;
     }
 }
@@ -32,6 +42,5 @@ int main()
             add_history(input);
         token = tokenizer(input);
         print_tokens(token);
-        
     }
 }
--- src/parsing/tokenizer.c
+++ src/parsing/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: yel-alja <yel-alja@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 15:17:00 by yel-alja          #+#    #+#             */
-/*   Updated: 2025/05/29 22:04:13 by yel-alja         ###   ########.fr       */
+/*   Updated: 2025/05/30 10:26:59 by yel-alja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ t_token *lst_new(char *input , t_type type)
     node->next = NULL;
     return (node);
 }
+
 void lst_addback(t_token **head, t_token *node)
 {
     t_token *tmp;
@@ -40,32 +41,36 @@ void lst_addback(t_token **head, t_token *node)
     }   
 }
 
-t_token *check_operator(char *input , int *i)
+t_token *_pipe(int *i)
 {
-    t_token *tmp = NULL;
-    int j;
-    int k = 0;
-    if(input[*i] == '|')
+    t_token *tmp;
+        
+    tmp = lst_new("|" , PIPE);
+    *i += 1;
+    return(tmp);
+}
+
+t_token *_heredoc_in(int *i , char c)
+{
+    t_token *tmp;
+    
+    if(c != '<' )
     {
-        tmp = lst_new("|" , PIPE);
+        tmp = lst_new("<" , INPUT);
         *i += 1;
     }
-    else if(input[*i] == '<')
+    else
     {
-        if(input[*i + 1] != '<' )
-        {
-            tmp = lst_new("<" , INPUT);
-            *i += 1;
-        }
-        else
-        {
-            tmp = lst_new("<<" , HEREDOC);
-            *i += 2;
-        }
+        tmp = lst_new("<<" , HEREDOC);
+        *i += 2;
     }
-    else if(input[*i] == '>')
-    {
-        if(input[*i + 1] != '>')
+    return (tmp);
+}
+
+t_token *_app_out(int *i , char c)
+{
+    t_token *tmp;
+    if(c != '>')
         {
             tmp = lst_new(">" , OUTPUT);
             *i += 1;
@@ -75,52 +80,87 @@ t_token *check_operator(char *input , int *i)
             tmp = lst_new(">>" , APPEND);
             *i += 2;
         }
+    return (tmp);
+}
+t_token *_double_quotes(char *input , int *i)
+{
+    int j;
+    int k = 0;
+    t_token *tmp;
+    
+    j = *i + 1;
+    while(input[j] && input[j] != '"')
+    {
+        j++;
+        k++;
     }
+    if(input[j] == '\0')
+        garbage_collect(NULL); //syntax error unclosed double quote" we should write it after this line
+    else
+    {
+        tmp = lst_new(ft_strndup(&input[*i + 1] , k - 1) , WORD);
+        *i += j + 1;
+    }
+    return (tmp);
+}
+
+t_token *_single_quotes(char *input , int *i)
+{
+      int j;
+    int k = 0;
+    t_token *tmp;
+    
+    j = *i + 1;
+    while(input[j] && input[j] != '\'')
+    {
+        j++;
+        k++;
+    }
+    if(input[j] == '\0')
+        garbage_collect(NULL); //syntax error unclosed double quote" we should write it after this line
+    else
+    {
+        tmp = lst_new(ft_strndup(&input[*i + 1] , k - 1) , WORD);
+        *i += j + 1;
+    }
+    return (tmp);
+}
+
+t_token *_word(char *input , int *i)
+{
+    t_token *tmp;
+    int j;
+    int k;
+    
+    j = *i;
+    k = 0;
+    while(input[j] && is_metachar(input[j]) == 0 && is_whitespace(input[j]) == 0)
+    {
+        j++;
+        k++;
+    }
+    tmp = lst_new(ft_strndup(&input[*i] , k - 1) , WORD);
+    *i = j;
+    return (tmp);
+}
+
+t_token *check_operator(char *input , int *i)
+{
+    t_token *tmp = NULL;
+    int j;
+    int k = 0;
+    if(input[*i] == '|')
+        tmp = _pipe(i);
+    else if(input[*i] == '<')
+        tmp = _heredoc_in(i , input[*i + 1]);
+    else if(input[*i] == '>')
+        tmp = _app_out(i , input[*i + 1]);
     else if(input[*i] == '"')
-    {
-      
-        j = *i + 1;
-        while(input[j] && input[j] != '"')
-        {
-            j++;
-            k++;
-        }
-        // if(input[j] == '\0')
-        //     garbage_collect(NULL); //syntax error unclosed double quote" we should write it after this line
-        // else
-        // {
-            tmp = lst_new(ft_strndup(&input[*i + 1] , k - 1) , WORD);
-            *i += j + 1;
-        // }
-    }
-    else if(input[*i] == '\'') // single quotes
-    {
-        j = *i + 1;
-        while(input[j] && input[j] != '\'')
-        {
-            j++;
-            k++;
-        }
-        // if(input[j] == '\0')
-        //     garbage_collect(NULL); //again here unclosed single quote
-        // else
-        // {
-            tmp = lst_new(ft_strndup(&input[*i + 1] , k - 1) , WORD);
-            *i += j + 1;
-        // }
-    }
+        tmp = _double_quotes(input , i);
+    else if(input[*i] == '\'')
+        tmp = _single_quotes(input , i);
     else // word
-    {
-        j = *i;
-        k = 0;
-        while(input[j] && is_metachar(input[j]) == 0 && is_whitespace(input[j]) == 0)
-        {
-            j++;
-            k++;
-        }
-        tmp = lst_new(ft_strndup(&input[*i] , k - 1) , WORD);
-        *i = j;
-    }
+        tmp = _word(input , i);
     return (tmp);
 }
 
