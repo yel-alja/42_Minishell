fab0fd304e2373adb07b4b2f6307d486d48dfa46
Zakaria Ouazrou <zouazrou@c1r9p3.1337.ma>
2025-07-20 10:55:32 +0100

    ✅norm header files

--- include/minishell.h
+++ include/minishell.h
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-alja <yel-alja@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 23:32:25 by yel-alja          #+#    #+#             */
-/*   Updated: 2025/07/20 10:06:11 by yel-alja         ###   ########.fr       */
+/*   Updated: 2025/07/20 10:50:48 by zouazrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <signal.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdbool.h>
-#include "../libft/libft.h"
-#include "parsing.h"
-#include "execution.h"
+# include <signal.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+# include "../libft/libft.h"
+# include "parsing.h"
+# include "execution.h"
 
-#define PROMPT "\033[0;35myzsh> \033[0m"
-#define SH_NAME "yzsh"
-#define SIZE_FILE_NAME 20
-#define WHITE_SP " \t\n"
+# define PROMPT "\033[0;35myzsh> \033[0m"
+# define SH_NAME "yzsh"
+# define SIZE_FILE_NAME 20
+# define WHITE_SP " \t\n"
 
 typedef struct s_env
 {
@@ -39,50 +39,50 @@ typedef struct s_env
 	struct s_env	*next;
 }					t_env;
 
-typedef enum	e_type
+typedef enum e_type
 {
-    PIPE,        // |x
-    WORD,        // "hello"
-    INPUT,       // <
-    OUTPUT,      // >
-    APPEND,      // >>
-    HEREDOC,     // <<x
+	PIPE,
+	WORD,
+	INPUT,
+	OUTPUT,
+	APPEND,
+	HEREDOC,
 	AMBG,
 }				t_type;
 
-typedef struct	s_redir
+typedef struct s_redir
 {
-    char			*filename;
-	int 			quoted;
-    t_type			type;
-    struct s_redir	*next;
+	char			*filename;
+	int				quoted;
+	t_type			type;
+	struct s_redir	*next;
 }					t_redir;
 
-typedef struct	s_cmd
+typedef struct s_cmd
 {
-	char 			*cmd;
+	char			*cmd;
 	char			**args;
 	int				fd_input;
 	int				fd_output;
 	bool			error;
-	t_redir 		*redirects;
+	t_redir			*redirects;
 	pid_t			pid;
 	struct s_cmd	*next;
 }					t_cmd;
 
 typedef struct s_token
 {
-	char *value;
-	t_type type;
-	int amg;
-	int quoted;
-	struct s_token *next;
+	char			*value;
+	t_type			type;
+	int				amg;
+	int				quoted;
+	struct s_token	*next;
 }	t_token;
 
 typedef struct s_garbage
 {
-	void *ptr;
-	struct s_garbage *next;
+	void				*ptr;
+	struct s_garbage	*next;
 }	t_garbage;
 
 /*******signal******/
@@ -90,8 +90,9 @@ void	ctrl_c(int sig);
 void	exe_pipeline_cmd(t_cmd *cmd);
 
 //?
-char *ft_getenv(char *name );
-char *expansion(char *token , int flag);
-char	*heredoc_file(char *del , int quoted);
+char	*ft_getenv(char *name);
+char	*expansion(char *token, int flag);
+char	*heredoc_file(char *del, int quoted);
 t_env	*ft_getvarenv(char *name);
+
 #endif
--- include/parsing.h
+++ include/parsing.h
@@ -3,64 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-alja <yel-alja@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 10:52:07 by yel-alja          #+#    #+#             */
-/*   Updated: 2025/07/20 01:12:55 by yel-alja         ###   ########.fr       */
+/*   Updated: 2025/07/20 10:54:25 by zouazrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
-#define PARSING_H
+# define PARSING_H
 
-#include "minishell.h"
+# include"minishell.h"
 
-typedef struct s_token t_token;
-typedef struct s_cmd t_cmd;
-typedef struct s_redir t_redir;
-typedef struct	s_env t_env;
-typedef enum	e_type t_type;
+typedef struct s_token	t_token;
+typedef struct s_cmd	t_cmd;
+typedef struct s_redir	t_redir;
+typedef struct s_env	t_env;
+typedef enum e_type		t_type;
 
 /*************token*************/
-t_token *tokenizer(char *input );
-t_token *new_token(char *input, t_type type , int amg);
-void token_add_back(t_token **head, t_token *node);
-int	contains_char(char c, char *str);
-t_token	*new_token(char *input, t_type type, int amg);
-void	token_add_back(t_token **head, t_token *node);
-void	update_amg(t_token *token);
-t_token	*build_list(char **res);
-t_token	*her_del(char *input, int *i);
-t_token	*token_re_input(int *i, char *c);
-t_token	*token_re_output(int *i, char c);
-char	*quoted_word(char *input, int *i, char *quote);
-char	*unquoted_word(char *input, int *i);
+t_token		*tokenizer(char *input );
+t_token		*new_token(char *input, t_type type, int amg);
+void		token_add_back(t_token **head, t_token *node);
+int			contains_char(char c, char *str);
+t_token		*new_token(char *input, t_type type, int amg);
+void		token_add_back(t_token **head, t_token *node);
+void		update_amg(t_token *token);
+t_token		*build_list(char **res);
+t_token		*her_del(char *input, int *i);
+t_token		*token_re_input(int *i, char *c);
+t_token		*token_re_output(int *i, char c);
+char		*quoted_word(char *input, int *i, char *quote);
+char		*unquoted_word(char *input, int *i);
 
 /*************garbage_collect*************/
-void garbage_collect(void *ptr ,bool terminate);
-
+void		garbage_collect(void *ptr, bool terminate);
 
 /*************syntax*************/
-int check_syntax(t_token *token);
-int check_quotes(char *input);
+int			check_syntax(t_token *token);
+int			check_quotes(char *input);
 
 /*************expantion*************/
 // char *expansion(char *token , t_env *env);
-int check_dollar(char *str);
+int			check_dollar(char *str);
 
 /*************utils*************/
-int is_whitespace(char c);
-int is_metachar(char c);
-int is_operator(t_token *token);
+int			is_whitespace(char c);
+int			is_metachar(char c);
+int			is_operator(t_token *token);
 
 /*************parser*************/
-void	cmd_add_back(t_cmd **head, t_cmd *node);
-t_cmd	*new_cmd(char *cmd ,char **args , t_redir *red);
-void	red_add_back(t_redir **head, t_redir *node);
-t_cmd	*parser(t_token *token );
-t_redir	*new_red(char *file , t_type type);
+void		cmd_add_back(t_cmd **head, t_cmd *node);
+t_cmd		*new_cmd(char *cmd, char **args, t_redir *red);
+void		red_add_back(t_redir **head, t_redir *node);
+t_cmd		*parser(t_token *token );
+t_redir		*new_red(char *file, t_type type);
 
 /******quote removal******/
-char *quote_removal(char *str);
+char		*quote_removal(char *str);
 
 #endif
